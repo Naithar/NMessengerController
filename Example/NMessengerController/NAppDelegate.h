c134ad663a13ac17e6a95241153cc6f33646846d
Naithar <devias.naith@gmail.com>
2015-04-23 00:30:30 +0300

    Initial commit

---
+++ Example/NMessengerController/NAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NAppDelegate.h
+//  NMessengerController
+//
+//  Created by CocoaPods on 04/23/2015.
+//  Copyright (c) 2014 Naithar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
